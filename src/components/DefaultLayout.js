5b56b95e79390cd1d71e6a41d00be2a3199647b7
Auxilia <auxiliavinnarasi@gmail.com>
2022-12-08 10:23:15 +0530

    sec com

---
+++ src/components/DefaultLayout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//all the pages we ll be havng the layout in top
+// in the page content we need to render the children,
+
+//children-which is nothing but the pages
+function DefaultLayout({children}) {
+  return (
+    <div>DefaultLayout</div>
+  )
+}
+
+export default DefaultLayout
