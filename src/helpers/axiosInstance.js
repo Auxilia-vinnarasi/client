ea12fe1d879e048be67c2c13e0f7cf839ab3c3f4
Auxilia <83941036+Auxilia-vinnarasi@users.noreply.github.com>
2023-01-17 11:08:26 +0530

    Update axiosInstance.js

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -5,6 +5,7 @@ import axios from "axios";
 export const axiosInstance=axios.create({
     //taking base url from the proxy..
     // baseURL:"http://localhost:5000/api",
+    baseURL:"https://tktbooking-backendd.onrender.com",
     headers:{
         Authorization:`Bearer ${localStorage.getItem("token")}`
     }
